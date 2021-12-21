3de015c38d9dc0ec05a5075c1d05403e26489fa1
jdonaldson40505 <don16009@byui.edu>
2021-12-21 01:59:31 -0700

    completed description

--- app/src/main/java/com/example/myapplication/description.kt
+++ app/src/main/java/com/example/myapplication/description.kt
@@ -1,4 +1,45 @@
 package com.example.myapplication
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.DescriptionBinding
 
-class description {
+
+class description:Fragment() {
+    private var _binding: DescriptionBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = DescriptionBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.update.setOnClickListener {
+
+            findNavController().navigate(R.id.to_profile)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
